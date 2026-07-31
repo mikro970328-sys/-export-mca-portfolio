a77418d6e6f55e25077141d19ee687fe54d3642c
mikro970328-sys <mikro970328@gmail.com>
2026-07-30 23:09:31 -0400

    Disable legacy Clients action menu loader

--- admin/erp.js
+++ admin/erp.js
@@ -120,9 +120,7 @@
         loadScript('/admin/workers-actions-menu.js', 'data-workers-actions-menu');
       });
 
-      loadScript('/admin/clients-module.js?v=20260730-1', 'data-clients-module', () => {
-        loadScript('/admin/client-actions-menu.js', 'data-client-actions-menu');
-      });
+      loadScript('/admin/clients-module.js?v=20260730-2', 'data-clients-module');
 
       loadScript('/admin/separate-container-tracking.js', 'data-separate-container-tracking', () => {
         loadScript('/admin/section-state.js?v=20260730-sessionfix1', 'data-section-state');
